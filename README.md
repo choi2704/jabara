dafe54ea8e164e4fdbb7088b5d14499d86e11655
choi2704 <takismjy@naver.com>
2026-07-07 13:45:57 +0900

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# javara
+# 강동자바라 자바라 견적계산기 v1
+
+## 계산 기준
+- SUS201 H1200: 80,000원/m
+- SUS201 H1500: 100,000원/m
+- SUS304 H1200: 120,000원/m
+- SUS304 H1500: 140,000원/m
+- 앵글바퀴: 추가금 없음
+- 60파이 우레탄: 추가금 없음
+- 100파이 우레탄: 5,000원/m 추가
+
+## 업로드 파일
+- index.html
+- jabara-v1.css
+- jabara-prices-v1.js
+- jabara-v1.js
+- README.md
+- .nojekyll
---
+++ jabara-prices-v1.js
@@ -0,0 +1,13 @@
+window.JABARA_TABLE = {
+  materials: ["SUS201", "SUS304"],
+  heights: ["H1200", "H1500"],
+  wheels: [
+    {name: "앵글바퀴", addPerM: 0},
+    {name: "60파이 우레탄", addPerM: 0},
+    {name: "100파이 우레탄", addPerM: 5000}
+  ],
+  prices: {
+    "SUS201": {"H1200": 80000, "H1500": 100000},
+    "SUS304": {"H1200": 120000, "H1500": 140000}
+  }
+};
